dac479ed8544082d877bfaea772130005ce56ed1
automationchad <will.marzella@tray.io>
2023-03-02 11:55:22 -0800

    changes

---
+++ components/consultingHero2.vue
@@ -0,0 +1,77 @@
+<template>
+	<div>
+		<!-- <div class="relative bg-white">
+			<div class="mx-auto max-w-7xl">
+				<div class="relative z-10 lg:w-full lg:max-w-2xl">
+					<svg
+						class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+						viewBox="0 0 100 100"
+						preserveAspectRatio="none"
+						aria-hidden="true"
+					>
+						<polygon points="0,0 90,0 50,100 0,100" />
+					</svg>
+
+					<div class="relative py-32 px-6 sm:py-40 lg:py-56 lg:px-8 lg:pr-0">
+						<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+							<div class="hidden sm:mb-10 sm:flex">
+								<div
+									class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+								>
+									Retainers to scale your business.
+									<a
+										href="#info"
+										class="whitespace-nowrap font-semibold text-indigo-600"
+										><span class="absolute inset-0" aria-hidden="true" />Read
+										more <span aria-hidden="true">&rarr;</span></a
+									>
+								</div>
+							</div>
+							<h1
+								class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+							>
+								<p>AutomationOps,</p>
+								<p>as a service.</p>
+							</h1>
+							<p class="mt-6 text-lg leading-8 text-gray-600">
+								We’re a process automation consultancy, providing advice and
+								delivery capability uplift to the worlds growing organisations.
+							</p>
+							<img
+								class="lg:aspect-auto mt-4 aspect-[3/2] rounded-lg object-cover lg:h-full lg:w-full"
+								src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+								alt=""
+							/>
+							<div class="mt-10 flex items-center gap-x-6">
+								<a
+									v-if="!profile.workspaces"
+									@click="showLoginModal = true"
+									class="cursor-pointer rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>Book a call</a
+								>
+								<a
+									v-else-if="profile.workspaces"
+									href="/consulting/contact"
+									class="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>Book a call</a
+								>
+								<a
+									href="#info"
+									class="text-sm font-semibold leading-6 text-gray-900"
+									>Learn more <span aria-hidden="true">→</span></a
+								>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+				<img
+					class="lg:aspect-auto aspect-[3/2] object-cover lg:h-full lg:w-full"
+					src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+					alt=""
+				/>
+			</div>
+		</div> -->
+	</div>
+</template>
--- pages/consulting/index.vue
+++ pages/consulting/index.vue
@@ -1,73 +1,127 @@
 <template>
 	<div class="">
-		<div class="relative bg-white">
-			<div class="mx-auto max-w-7xl">
-				<div class="relative z-10 lg:w-full lg:max-w-2xl">
-					<svg
-						class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
-						viewBox="0 0 100 100"
-						preserveAspectRatio="none"
-						aria-hidden="true"
-					>
-						<polygon points="0,0 90,0 50,100 0,100" />
-					</svg>
-
-					<div class="relative py-32 px-6 sm:py-40 lg:py-56 lg:px-8 lg:pr-0">
-						<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-							<div class="hidden sm:mb-10 sm:flex">
-								<div
-									class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+		<div class="bg-white">
+			<div
+				class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20"
+			>
+				<div
+					class="mx-auto max-w-7xl pt-10 pb-24 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:py-40 lg:px-8"
+				>
+					<div class="px-6 lg:px-0 lg:pt-4">
+						<div class="mx-auto max-w-2xl">
+							<div class="max-w-lg">
+								<img
+									class="h-11"
+									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&amp;shade=600"
+									alt="Your Company"
+								/>
+								<div class="mt-24 sm:mt-32 lg:mt-16">
+									<a href="#" class="inline-flex space-x-6">
+										<span
+											class="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10"
+											>Retainers to scale your business.</span
+										>
+										<span
+											class="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600"
+										>
+											<span>Read more</span>
+											<ChevronRightIcon
+												class="h-5 w-5 text-gray-400"
+												aria-hidden="true"
+											/>
+										</span>
+									</a>
+								</div>
+								<h1
+									class="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
 								>
-									Retainers to scale your business.
+									<p>AutomationOps,</p>
+									<p>as a service.</p>
+								</h1>
+								<p class="mt-6 text-lg leading-8 text-gray-600">
+									We’re a process automation consultancy, providing advice and
+									delivery capability uplift to the worlds growing
+									organisations.
+								</p>
+								<div class="mt-10 flex items-center gap-x-6">
+									<a
+										v-if="!profile.workspaces"
+										@click="showLoginModal = true"
+										class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+										>Book a call</a
+									>
+									<a
+										v-else-if="profile.workspaces"
+										href="/consulting/contact"
+										class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+										>Book a call</a
+									>
 									<a
 										href="#info"
-										class="whitespace-nowrap font-semibold text-indigo-600"
-										><span class="absolute inset-0" aria-hidden="true" />Read
-										more <span aria-hidden="true">&rarr;</span></a
+										class="text-sm font-semibold leading-6 text-gray-900"
+										>Learn more <span aria-hidden="true">→</span></a
 									>
 								</div>
 							</div>
-							<h1
-								class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+						</div>
+					</div>
+					<div
+						class="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen"
+					>
+						<div
+							class="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36"
+							aria-hidden="true"
+						/>
+						<div class="shadow-lg md:rounded-3xl">
+							<div
+								class="bg-indigo-500 [clip-path:inset(0)] md:[clip-path:inset(0_round_theme(borderRadius.3xl))]"
 							>
-								<p>AutomationOps,</p>
-								<p>as a service.</p>
-							</h1>
-							<p class="mt-6 text-lg leading-8 text-gray-600">
-								We’re a process automation consultancy, providing advice and
-								delivery capability uplift to the worlds growing organisations.
-							</p>
-							<div class="mt-10 flex items-center gap-x-6">
-								<a
-									v-if="!profile.workspaces"
-									@click="showLoginModal = true"
-									class="cursor-pointer rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-									>Book a call</a
-								>
-								<a
-									v-else-if="profile.workspaces"
-									href="/consulting/contact"
-									class="rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-									>Book a call</a
-								>
-								<a
-									href="#info"
-									class="text-sm font-semibold leading-6 text-gray-900"
-									>Learn more <span aria-hidden="true">→</span></a
-								>
+								<div
+									class="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36"
+									aria-hidden="true"
+								/>
+								<div class="relative px-6 pt-8 sm:pt-16 md:pl-16 md:pr-0">
+									<div class="mx-auto max-w-2xl md:mx-0 md:max-w-none">
+										<div
+											class="w-screen overflow-hidden rounded-tl-xl bg-gray-900"
+										>
+											<div class="flex bg-gray-800/40 ring-1 ring-white/5">
+												<div
+													class="-mb-px flex text-sm font-medium leading-6 text-gray-400"
+												>
+													<div
+														class="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 py-2 px-4 text-white"
+													>
+														NotificationSetting.robot
+													</div>
+													<div class="border-r border-gray-600/10 py-2 px-4">
+														App.jsx
+													</div>
+												</div>
+											</div>
+											<div class="px-6 pt-6 pb-14">
+												<pre class="text-[0.8125rem] leading-6 text-gray-300">export default</pre>
+											</div>
+										</div>
+									</div>
+									<div
+										class="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 md:rounded-3xl"
+										aria-hidden="true"
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-				<img
-					class="lg:aspect-auto aspect-[3/2] object-cover lg:h-full lg:w-full"
-					src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
-					alt=""
+				<div
+					class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"
 				/>
 			</div>
 		</div>
+		<Testimonials
+			:featured-testimonial="featured_case_study"
+			:testimonials="case_studies.filter((o) => !o.fields.featured)"
+		/>
 
 		<div
 			class="relative isolate overflow-hidden bg-white py-24 sm:py-32"
@@ -97,6 +151,7 @@
 					</linearGradient>
 				</defs>
 			</svg>
+
 			<div class="mx-auto max-w-7xl px-6 lg:px-8">
 				<div class="mx-auto max-w-2xl lg:mx-0">
 					<p
@@ -114,6 +169,7 @@
 						exposing you to the complexity that actually makes it work.
 					</p>
 				</div>
+
 				<div
 					class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:mx-0 lg:mt-10 lg:max-w-none lg:grid-cols-12"
 				>
@@ -347,12 +403,6 @@
 				</div>
 			</div>
 		</div>
-		
-
-		<Testimonials
-			:featured-testimonial="featured_case_study"
-			:testimonials="case_studies.filter((o) => !o.fields.featured)"
-		/>
 
 		<Team />
 		<consulting-pricing
@@ -459,6 +509,7 @@
 		MinusSmallIcon,
 		PlusSmallIcon,
 		ExclamationTriangleIcon,
+		ChevronRightIcon
 	} from '@heroicons/vue/24/outline';
 
 	import Airtable from 'airtable';
