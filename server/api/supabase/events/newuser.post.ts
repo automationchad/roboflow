bc7d9e6c9c09e92b2aab1a726fd1aae1531ea879
automationchad <will.marzella@tray.io>
2023-03-05 23:02:26 -0800

    errors

--- server/api/supabase/events/newuser.post.ts
+++ server/api/supabase/events/newuser.post.ts
@@ -6,14 +6,14 @@ export default defineEventHandler(async (event) => {
 	const supabase = serverSupabaseClient(event);
 	const body = await readBody(event);
 	const email_domain = body.record.username.split('@')[1];
-	let { data: workspaces, error } = await supabase
+	let { data: workspaces } = await supabase
 		.from('workspaces')
 		.select('*')
 		.eq('domain', email_domain)
 		.limit(1)
 		.single();
 	if (workspaces) {
-		const { data, error } = await supabase
+		const { data } = await supabase
 			.from('profiles')
 			.update({ workspace_id: workspaces.id })
 			.eq('id', body.record.id);
@@ -25,7 +25,7 @@ export default defineEventHandler(async (event) => {
 				userId: body.record.id,
 			},
 		});
-		const { data: workspaces, error } = await supabase
+		const { data: workspaces } = await supabase
 			.from('workspaces')
 			.insert([
 				{
@@ -38,7 +38,7 @@ export default defineEventHandler(async (event) => {
 				},
 			])
 			.select();
-		const { data, error } = await supabase
+		const { data } = await supabase
 			.from('profiles')
 			.update({ workspace_id: workspaces.id })
 			.eq('id', body.record.id);
