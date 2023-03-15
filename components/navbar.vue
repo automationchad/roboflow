9ef0a71a2b1a83cfafc203cde7c4d431e4bcedaf
automationchad <will.marzella@tray.io>
2023-03-15 14:22:01 -0700

    navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,12 +11,9 @@
 	import logo from '~/assets/images/logo.png';
 	import { ShoppingCartIcon, ChevronDownIcon } from '@heroicons/vue/24/outline';
 	import { PlusIcon } from '@heroicons/vue/20/solid';
-
-	const props = defineProps({
-		profile: {
-			type: Object,
-		},
-	});
+	import { useAttrs } from 'vue';
+	const attrs = useAttrs();
+	const profile = attrs.profile;
 
 	const user = useSupabaseUser();
 	const supabase = useSupabaseClient();
@@ -27,7 +24,7 @@
 		try {
 			loading.value = true;
 			let { error } = await supabase.auth.signOut();
-			location.reload()
+			location.reload();
 			if (error) throw error;
 		} catch (error) {
 			alert(error.message);
@@ -89,36 +86,11 @@
 		<div class="mx-auto px-48">
 			<div class="flex h-16 justify-between">
 				<div class="flex">
-					<div class="-ml-2 mr-2 flex items-center md:hidden">
-						<!-- Mobile menu button -->
-						<DisclosureButton
-							class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-						>
-							<span class="sr-only">Open main menu</span>
-						</DisclosureButton>
-					</div>
 					<div class="flex flex-shrink-0 items-center">
 						<a href="/"
 							><img class="block h-8 w-auto lg:hidden" :src="logo" />
 							<img class="hidden h-8 w-auto lg:block" :src="logo"
 						/></a>
-					</div>
-					<div class="hidden md:ml-6 md:flex md:space-x-8">
-						<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-						<DisclosureButton
-							v-for="path in paths"
-							:key="path.id"
-							as="a"
-							:href="path.id"
-							:target="path.target"
-							:class="[
-								path.active
-									? 'border-indigo-500 text-slate-900 dark:text-white'
-									: 'border-transparent text-slate-500 hover:border-slate-300 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300',
-								'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium transition-colors',
-							]"
-							>{{ path.title }}</DisclosureButton
-						>
 					</div>
 				</div>
 				<div
@@ -226,80 +198,11 @@
 				</div>
 			</div>
 		</div>
-
-		<DisclosurePanel class="md:hidden">
-			<div class="space-y-1 pt-2 pb-3">
-				<!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-				<DisclosureButton
-					v-for="path in paths"
-					:key="path.id"
-					as="a"
-					:href="path.id"
-					:class="[
-						path.active
-							? 'border-indigo-500 bg-indigo-50 text-indigo-700'
-							: 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700',
-						'block border-l-4 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-gray-500  transition-colors sm:pl-5 sm:pr-6',
-					]"
-					>{{ path.title }}</DisclosureButton
-				>
-			</div>
-			<div class="border-t border-gray-200 pt-4 pb-3">
-				<div class="flex items-center px-4 sm:px-6">
-					<div class="flex-shrink-0">
-						<img
-							class="h-10 w-10 rounded-full"
-							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-							alt=""
-						/>
-					</div>
-					<div class="ml-3">
-						<div class="text-base font-medium text-gray-800">Tom Cook</div>
-						<div class="text-sm font-medium text-gray-500">tom@example.com</div>
-					</div>
-					<button
-						type="button"
-						class="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-					>
-						<span class="sr-only">View notifications</span>
-						<ShoppingCartIcon class="h-6 w-6" aria-hidden="true" />
-					</button>
-				</div>
-				<div class="mt-3 space-y-1">
-					<DisclosureButton
-						as="a"
-						href="#"
-						class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-						>Your Profile</DisclosureButton
-					>
-					<DisclosureButton
-						as="a"
-						href="#"
-						class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-						>Settings</DisclosureButton
-					>
-					<DisclosureButton
-						as="a"
-						href="#"
-						class="block bg-black px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-						>Sign out</DisclosureButton
-					>
-				</div>
-			</div>
-		</DisclosurePanel>
 	</Disclosure>
 </template>
 
 <script>
 	export default {
-		props: {
-			user: {
-				type: Object,
-			},
-			profile: {
-				type: Object,
-			},
-		},
 		components: { ChevronDownIcon },
 	};
 </script>
