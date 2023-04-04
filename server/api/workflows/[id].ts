877713368bf32dee25a12564d3144df88d7cd0b3
automationchad <will.marzella@tray.io>
2023-04-04 13:13:50 -0700

    admin

--- server/api/workflows/[id].ts
+++ server/api/workflows/[id].ts
@@ -1,5 +1,16 @@
+import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server';
+
 export default defineEventHandler(async (event) => {
 	const query = await getQuery(event);
+	const user = await serverSupabaseUser(event);
+	const supabase = await serverSupabaseClient(event);
+
+	let { data: User, error: userError } = await supabase
+		.from('User')
+		.select('systemRole,defaultTeamId,Account(*,Subscription(*),Ticket(count))')
+		.eq('id', user.id)
+		.limit(1)
+		.single();
 	var date = new Date(Date.now());
 	var firstDay = new Date(date.getFullYear(), 1, 1);
 	var d = new Date(new Date().getFullYear(), 0, 1);
@@ -7,7 +18,10 @@ export default defineEventHandler(async (event) => {
 	const tray_key = test
 		? 'bf2d37099d0b4d59bdf52ee88f05faef222946f102064753abc50be648775156'
 		: 'c872e7dfb9404c1a8c29757b8e5715e0d20af44d95264c819be3fdab09f1f447';
-	const filters = test ? {} : { workspaces: [event.context.params.id] };
+	const filters =
+		User?.Account.type === 'super_admin'
+			? {}
+			: { workspaces: [event.context.params.id] };
 	const data = await $fetch('https://api.tray.io/insights/v1/list/workflows', {
 		method: 'post',
 		headers: {
