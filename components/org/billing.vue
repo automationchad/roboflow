0e1333561f997a2cd4aacffe47e71441bb5e4859
automationchad <will.marzella@tray.io>
2023-04-10 17:10:00 -0700

    billing

--- components/org/billing.vue
+++ components/org/billing.vue
@@ -21,54 +21,73 @@
 									Billing information
 								</h2>
 							</div> -->
-
-							<div class="grid grid-cols-4 gap-6">
-								<div class="col-span-4 sm:col-span-2">
-									<label
-										for="email-address"
-										class="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
-										>Billing email</label
-									>
-
+							<label
+								for="email-address"
+								class="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
+								>Billing email</label
+							>
+							<Disclosure
+								as="li"
+								v-slot="{ open }"
+								class="flex items-start justify-between"
+							>
+								<div class="w-full" v-if="!open">
 									<div
-										class="mt-2 block rounded-md border-0 dark:bg-slate-800 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 dark:bg-slate-800 dark:text-white dark:ring-slate-700 sm:text-sm sm:leading-6"
+										class="block rounded-md border-0 px-3 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 dark:bg-slate-800 dark:bg-slate-800 dark:text-white dark:ring-slate-700 sm:text-sm sm:leading-6"
 									>
-										
-										<input
-											
-											v-model="state.email"
-											type="text"
-											name="email-address"
-											id="email-address"
-											autocomplete="email"
-											class="w-full border-none bg-transparent p-0 text-sm focus:ring-0"
-										/>
+										{{ state.email }}
 									</div>
+									
+								</div>
 
-									<p class="mt-2 text-xs text-slate-600 dark:text-slate-300">
+								<DisclosurePanel
+									class="flex flex-grow items-center justify-between text-sm text-gray-500"
+								>
+									<div class="w-full">
+										<div class="flex justify-between items-center">
+											<div
+												class="block mr-2 w-full rounded-md border-0 px-3 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 dark:bg-slate-800 dark:bg-slate-800 dark:text-white dark:ring-slate-700 sm:text-sm sm:leading-6"
+											>
+												<input
+													v-model="state.email"
+													type="text"
+													name="email-address"
+													id="email-address"
+													autocomplete="email"
+													class="w-full border-none bg-transparent p-0 text-sm focus:ring-0"
+												/>
+											</div>
+											<div class="flex items-center space-x-2">
+												<DisclosureButton
+													@click="handleReset()"
+													class="rounded-md border border-indigo-600 bg-white p-2 text-indigo-600"
+												>
+													<XMarkIcon class="h-5 w-5" />
+												</DisclosureButton>
+												<DisclosureButton
+													@click="
+														handleUpdateStripe(User.Account.stripeCustomerId, {
+															email: state.email,
+														})
+													"
+													:disabled="loading"
+													class="rounded-md bg-indigo-600 p-2 text-white"
+												>
+													<CheckIcon class="h-5 w-5" />
+												</DisclosureButton>
+											</div>
+										</div>
+									</div>
+								</DisclosurePanel>
+								<div class="ml-2">
+									<DisclosureButton class="p-2" v-if="!open">
+										<PencilIcon class="h-4 w-4 dark:text-slate-400" />
+									</DisclosureButton>
+								</div>
+							</Disclosure>
+							<p class="mt-2 text-xs text-slate-600 dark:text-slate-300">
 										The invoices will be sent to this email
 									</p>
-								</div>
-							</div>
-						</div>
-						<div class="space-x-4 px-4 py-3 text-right sm:px-6">
-							<button
-								@click="handleReset()"
-								class="inline-flex justify-center rounded-md border border-slate-400 py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 dark:text-slate-300"
-							>
-								Cancel
-							</button>
-							<button
-								@click="
-									handleUpdateStripe(User.Account.stripeCustomerId, {
-										email: state.email,
-									})
-								"
-								:disabled="loading"
-								class="inline-flex justify-center rounded-md bg-gray-900 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 dark:bg-slate-700"
-							>
-								{{ loading ? 'Loading' : 'Submit' }}
-							</button>
 						</div>
 					</div>
 				</fieldset>
@@ -99,6 +118,7 @@
 		MagnifyingGlassIcon,
 		QuestionMarkCircleIcon,
 		XCircleIcon,
+		CheckIcon,
 	} from '@heroicons/vue/20/solid';
 	import {
 		Bars3Icon,
@@ -111,6 +131,7 @@
 		SquaresPlusIcon,
 		UserCircleIcon,
 		XMarkIcon,
+		PencilIcon,
 	} from '@heroicons/vue/24/outline';
 
 	import { format } from 'date-fns';
