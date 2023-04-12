6f54ff3e811dbff33eccc3f76afec9afedac7cb6
automationchad <will.marzella@tray.io>
2023-04-12 00:37:21 -0700

    resumes at

--- server/api/stripe/subscription/resume.ts
+++ server/api/stripe/subscription/resume.ts
@@ -3,7 +3,7 @@ const stripe = Stripe(process.env.STRIPE_KEY);
 
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
-	const resumes_at = body.days_left + Math.round(new Date().getTime() / 1000);
+	const resumes_at = (body.days_left + Math.round(new Date().getTime())) / 1000;
 	const subscription = await stripe.subscriptions.update(body.subscriptionId, {
 		pause_collection: { behavior: 'void', resumes_at },
 	});
